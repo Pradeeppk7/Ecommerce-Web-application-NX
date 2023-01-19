f7e4bef5d1920e0a54bd21dc61e032c4fcb5b4b4
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2023-01-20 00:34:20 +0530

    cart page ui

--- libs/orders/src/index.ts
+++ libs/orders/src/index.ts
@@ -5,4 +5,3 @@ export * from './lib/models/order-item';
 export * from './lib/services/orders.service';
 export * from './lib/services/cart.service';
 export * from './lib/models/cart';
-
--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Route } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { ordersRoutes } from './lib.routes';
 import { CartService } from './services/cart.service';
 import { CartIconComponent } from './components/cart-icon/cart-icon.component';
 import { BadgeModule } from 'primeng/badge';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { ButtonModule } from 'primeng/button';
+import {InputNumberModule} from 'primeng/inputnumber'
 
+const routes: Routes = [
+  {
+    path: 'cart',
+    component: CartPageComponent,
+  },
+];
 @NgModule({
-  imports: [CommonModule, RouterModule, BadgeModule],
-  declarations: [CartIconComponent],
-  exports: [CartIconComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BadgeModule,
+    InputNumberModule,
+    ButtonModule,
+    RouterModule.forChild(routes),
+  ],
+  declarations: [CartIconComponent, CartPageComponent],
+  exports: [CartIconComponent, CartPageComponent],
 })
 export class OrdersModule {
   constructor(cartService: CartService) {
